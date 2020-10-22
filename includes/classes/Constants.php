05cbf85c5dbd80d782f7279db57b0cc2a74e58ee
Anna Astori <anastori@tetrika.school>
2020-10-22 11:09:59 +0300

    Add signup

---
+++ includes/classes/Constants.php
@@ -0,0 +1,12 @@
+<?php
+  class Constants {
+    public static $usernameLength = "You username must be between 5 and 25 characters";
+    public static $firstNameLength = "You first name must be between 2 and 25 characters";
+    public static $lastNameLength = "You last name must be between 2 and 25 characters";
+    public static $emailsDontMatch = "Your emails don't match";
+    public static $emailInvalid = "Email is invalid";
+    public static $passwordsDontMatch = "Your passwords don't match";
+    public static $passwordLettersAndNumbers = "Your password can only contain letters and numbers";
+    public static $passwordLength = "You password must be between 5 and 30 characters";
+  }
+ ?>
