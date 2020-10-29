cf889beafbe3dd5d7ea9e072e3011da61a86fe21
Anna Astori <anastori@tetrika.school>
2020-10-29 11:00:58 +0300

    Add playing from playlist

--- includes/header.php
+++ includes/header.php
@@ -19,8 +19,17 @@ if (isset($_SESSION['userLoggedIn'])) {
     <meta charset="utf-8">
     <title>Spotify-clone</title>
     <link rel="stylesheet" href="assets/css/style.css">
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+    <script src="assets/js/script.js"></script>
   </head>
   <body>
+
+    <script>
+      var audioElement = new Audio();
+      audioElement.setTrack("assets/music/bensound-acousticbreeze.mp3");
+      // audioElement.audio.play();
+    </script>
     <div id="mainContainer">
 
       <div id="topContainer">
