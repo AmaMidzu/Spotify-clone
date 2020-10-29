d0c23a8a4f9077828c06f83a495b030f379afd17
Anna Astori <anastori@tetrika.school>
2020-10-29 22:16:04 +0300

    Add control for playback progress bar

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -15,7 +15,32 @@ $jsonArray = json_encode($resultArray);
     currentPlaylist = <?php echo $jsonArray; ?>;
     audioElement = new Audio();
     setTrack(currentPlaylist[0], currentPlaylist, false);
+
+    $(".playbackBar .progressBar").mousedown(function() {
+      mouseDown = true;
+    });
+
+    $(".playbackBar .progressBar").mousemove(function(e) {
+      if (mouseDown) {
+        timeFromOffset(e, this);
+      }
+    });
+
+    $(".playbackBar .progressBar").mouseup(function(e) {
+      timeFromOffset(e, this);
+    });
+
+    $(document).mouseup(function() {
+      mouseDown = false;
+    });
+
   });
+
+  function timeFromOffset(mouse, progressBar) {
+    var percentage = mouse.offsetX / $(progressBar).width() * 100; //just for clarity that it's fraction
+    var seconds = audioElement.audio.duration * (percentage / 100);
+    audioElement.setTime(seconds);
+  }
 
   function setTrack(trackId, newPlaylist, play) {
     $.post("includes/handlers/ajax/getSongJson.php", { songId: trackId }, function(data) {
@@ -35,8 +60,8 @@ $jsonArray = json_encode($resultArray);
         $(".albumLink img").attr("src", album.artworkPath)
       });
 
-      audioElement.setTrack(track.path);
-      // audioElement.play();
+      audioElement.setTrack(track);
+      playSong();
     });
     if (play) {
       audioElement.play();
@@ -44,6 +69,9 @@ $jsonArray = json_encode($resultArray);
   }
 
   function playSong() {
+    if (audioElement.audio.currentTime == 0) {
+      $.post("includes/handlers/ajax/updatePlays.php", { songId: audioElement.currentlyPlaying.id });
+    }
     $(".controlButton.play").hide();
     $(".controlButton.pause").show();
     audioElement.play();
