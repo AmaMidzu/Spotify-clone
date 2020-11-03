4be1ffce45287d16261e8d8fd9cfac06dda0c407
Anna Astori <anastori@tetrika.school>
2020-11-03 23:58:27 +0300

    Add links

--- includes/nowPlayingBar.php
+++ includes/nowPlayingBar.php
@@ -150,19 +150,19 @@ $jsonArray = json_encode($resultArray);
     $.post("includes/handlers/ajax/getSongJson.php", { songId: trackId }, function(data) {
 
       var track = JSON.parse(data);
-
       $(".trackName span").text(track.title);
 
       $.post("includes/handlers/ajax/getArtistJson.php", { artistId: track.artist}, function(data) {
         var artist = JSON.parse(data);
-
         $(".artistName span").text(artist.name)
+        $(".artistName span").attr("onclick", "openPage('artist.php?id=" + artist.id + "')");
       });
 
       $.post("includes/handlers/ajax/getAlbumJson.php", { albumId: track.album}, function(data) {
         var album = JSON.parse(data);
-
-        $(".albumLink img").attr("src", album.artworkPath)
+        $(".albumLink img").attr("src", album.artworkPath);
+        $(".albumLink img").attr("onclick", "openPage('album.php?id=" + album.id + "')");
+        $(".trackName span").attr("onclick", "openPage('album.php?id=" + album.id + "')");
       });
 
       audioElement.setTrack(track);
@@ -194,16 +194,16 @@ $jsonArray = json_encode($resultArray);
     <div id="nowPlayingLeft">
       <div class="content">
         <span class="albumLink">
-          <img class="albumArtwork" src="">
+          <img role="link" tabindex="0" class="albumArtwork" src="">
         </span>
         <div class="trackInfo">
 
           <span class="trackName">
-            <span></span>
+            <span role="link" tabindex="0"></span>
           </span>
 
           <span class="artistName">
-            <span>Default artist</span>
+            <span role="link" tabindex="0"></span>
           </span>
 
         </div>
