62462b09c7e4add62c6d5d6c91b15dc72fdd3858
Anna Astori <anastori@tetrika.school>
2020-11-13 02:58:19 +0300

    Add settings page

--- includes/navBarContainer.php
+++ includes/navBarContainer.php
@@ -27,7 +27,7 @@
       </div>
 
       <div class="navItem">
-        <span role="link" tabindex="0" onclick="openPage('profile.php')"  class="navItemLink">Your profile</span>
+        <span role="link" tabindex="0" onclick="openPage('settings.php')"  class="navItemLink"><?php echo $userLoggedIn->getUsername(); ?></span>
       </div>
     </div>
 
